2c4adbdd68fb7209e2290d33c3834e3115522339
Tjalle <31376864+tjallo@users.noreply.github.com>
2021-02-10 20:53:00 +0100

    using clicking you can set the angle

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -12,13 +12,14 @@ export default {
   components: {},
   mounted() {
     const script = (p5) => {
+      let clicked = false;
+      let a1 = 0;
       const windowHeight = 1000;
       const windowWidth = 1000;
       const PI = Math.PI;
       const arcSize = 0.75;
       const radius = windowWidth / 2 - 0.5 * windowWidth * (1 - arcSize);
       const colWidth = 0.04;
-      // eslint-disable-next-line no-unused-vars
       const twoPointerAngles = [
         [
           -PI * (colWidth * -3 + colWidth / 2),
@@ -56,6 +57,7 @@ export default {
       p5.setup = () => {
         p5.createCanvas(windowHeight, windowWidth);
         // p5.frameRate(2);
+        p5.determinePoints(40);
       };
       p5.draw = () => {
         p5.clear();
@@ -76,7 +78,19 @@ export default {
         // let randNr = p5.random(maxRange[0], maxRange[1]);
         let pos1 = -0.5 * PI + centreOffset;
         p5.arc_range(pos1, colWidth);
-        p5.drawMouse();
+        p5.untilClick();
+      };
+
+      p5.untilClick = () => {
+        if (clicked == false) {
+          p5.drawMouse();
+        } else {
+          p5.circleLine(a1);
+        }
+      };
+
+      p5.mouseClicked = () => {
+        clicked = !clicked;
       };
 
       p5.arc_range = (start, colWidth) => {
@@ -109,24 +123,8 @@ export default {
       };
 
       p5.drawMouse = () => {
-        // p5.noStroke();
-        // p5.ellipse(p5.mouseX, p5.mouseY, 40, 40);
-        // p5.line(p5.mouseX, p5.mouseY, p5.mouseX + 20, p5.mouseY + 20);
-        // p5.line(200, 200, windowWidth / 2, windowHeight / 2);
-
-        // let radius = windowWidth / 2 - (windowWidth / 2) * arcSize;
-        // let xPos = 0.5 * windowWidth * (1 - arcSize);
-
-        // p5.line(xPos, windowHeight, xPos, 0);
-
-        // let cur = colWidth * -2 + colWidth / 2;
-
-        // eslint-disable-next-line no-unused-vars
-
-        let a1 = p5.calculateAngle();
-        // console.log(a1)
+        a1 = p5.calculateAngle();
         p5.circleLine(a1);
-
       };
 
       // The Range is between PI * [-0.5, 0.5]
@@ -152,7 +150,27 @@ export default {
       };
 
       // eslint-disable-next-line no-unused-vars
-     
+      p5.determinePoints = (angle) => {
+        if (p5.checkIfInbetween(twoPointerAngles, angle)) {
+          return 2;
+        } else if (p5.checkIfInbetween(threePointerAngles, angle)) {
+          return 3;
+        } else if (p5.checkIfInbetween(fourPointerAngles, angle)) {
+          return 4;
+        }
+        return 0;
+      };
+      p5.checkIfInbetween = (angleArray, angle) => {
+        angleArray.forEach((x) => {
+          let minval = Math.min.apply(null, x);
+          let maxval = Math.max.apply(null, x);
+          if (angle > minval && angle < maxval) {
+            return true;
+          }
+        });
+
+        return false;
+      };
     }; // Attach the canvas to the div
 
     // eslint-disable-next-line no-unused-vars
